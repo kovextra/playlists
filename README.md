1ac6ee9e275fbcf3933c1e16d5729632bdfe15dc
Kovey Coles <kovextra@gmail.com>
2025-11-21 16:18:52 -0800

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-A project for music lovers who want dynamic ways to manage their playlists
--- src/components/CenterDisplay.jsx
+++
@@ -1,9 +0,0 @@
-function CenterDisplay({ props }) {
-  return (
-    <div className="center-display shadow-md">
-      <div className="p-5 font-medium m-5"></div>
-      {props}
-    </div>
-  );
-}
-export default CenterDisplay;
